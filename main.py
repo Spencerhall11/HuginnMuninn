fb9cbbbfa93578109a6604fdfd96c4575993a315
shall053 <shall053@odu.edu>
2026-06-27 17:36:41 -0400

    Update main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+#imports
+import threading
+
+#declare
+def main():
+    #state its starting
+    print("Huginn and Muninn released")
+    #try except
+    try:
+        pass
+    except KeyboardInterrupt:
+        #message for shutdown
+        print("Huginn and Muninn have returned")
+
+
+#run it
+if __name__ =="__main__":
+    main()
